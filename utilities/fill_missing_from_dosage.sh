d4c46e346be01c9c07a78b359d0894595007b2f5
Saul Pierotti <saul@ebi.ac.uk>
2024-02-27 17:49:02 +0000

    Update fill_missing_from_dosage.sh

--- utilities/fill_missing_from_dosage.sh
+++ utilities/fill_missing_from_dosage.sh
@@ -7,14 +7,20 @@
 N_CHR=24 # 24 for medaka
 VCF="$1" # vcf from the command line
 DOSAGE_FIELD="DS"
+MEM="10000"
+NCPU="10"
 
 plink2 \
+  --memory $MEM \
+  --threads $NCPU \
   --out tmp \
   --chr-set 24 \
   --vcf $VCF dosage=$DOSAGE_FIELD \
   --make-pgen vzs fill-missing-from-dosage erase-dosage
 
 plink2 \
+  --memory $MEM \
+  --threads $NCPU \
   --out "$(basename $VCF).no_missing"\
   --chr-set 24 \
   --pgen tmp.pgen
